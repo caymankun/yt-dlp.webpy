fc078f0392449ff7b880982d2602d8167970a2b5
caymankun <111965236+caymankun@users.noreply.github.com>
2024-02-06 21:02:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,49 @@
-from flask import Flask, request, jsonify
-import subprocess
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
+from datetime import datetime
+import yt_dlp
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-def embed_thumbnail_url(video_url, thumbnail_url):
-    # 動画URLにサムネイルURLをクエリパラメータとして追加
-    embedded_url = f"{video_url}?thumbnail={thumbnail_url}"
-    return embedded_url
+@app.route('/download', methods=['GET'])
+def download_media():
+    media_url = request.args.get('url')
+    media_type = request.args.get('type')  # 'audio' or 'video'
 
-@app.route('/', methods=['POST'])
-def download_video():
-    if request.method == 'POST':
-        video_url = request.form['video_url']
-        format = "mp4"
-        
-        # サムネイルURLをリクエストから取得
-        thumbnail_url = request.form.get('thumbnail_url', '')  # 'thumbnail_url' を取得
-        
-        try:
-            # yt-dlpコマンドを実行してダウンロードURLを取得
-            command = ["yt-dlp", "--format", format, "--get-url", video_url]
-            result = subprocess.run(command, capture_output=True, text=True, check=True)
-            download_url = result.stdout.strip()
-            
-            if download_url:
-                # サムネイルを埋め込んだURLを生成
-                embedded_url = embed_thumbnail_url(download_url, thumbnail_url)
-                return jsonify({"download_url": download_url, "embedded_url": embedded_url})
-            else:
-                return jsonify({"error": "ダウンロードURLの取得に失敗しました。"})
-        except subprocess.CalledProcessError as e:
-            return jsonify({"error": str(e)})
+    if not media_url or not media_type:
+        return jsonify({'error': 'URL or type parameter is missing'}), 400
+
+    try:
+        now = datetime.now()
+        formatted_now = now.strftime('%Y%m%d%H%M%S')
+        output_file_name = f'output_{formatted_now}'
+
+        if media_type == 'audio':
+            ydl_opts = {
+                'format': '22',
+                'outtmpl': f'/tmp/{output_file_name}.mp3',
+                'embed-thumbnail': True,
+                'add-metadata': True,
+            }
+        elif media_type == 'video':
+            ydl_opts = {
+                'format': '22',
+                'outtmpl': f'/tmp/{output_file_name}.mp4',
+                'embed-thumbnail': True,
+            }
+        else:
+            return jsonify({'error': 'Invalid media type'}), 400
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([media_url])
+
+        file_path = f'/tmp/{output_file_name}.{media_type}'
+        return send_file(file_path, as_attachment=True, attachment_filename=f'{output_file_name}.{media_type}')
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True)
